cb636c5ef2a04acd5e52dc2b082157487ec71a3c
Michael Yoshimura <miyo6032@colorado.edu>
2021-03-09 20:44:39 -0700

    Update version

--- src/main/java/com/barribob/totemmod/TotemConstants.java
+++ src/main/java/com/barribob/totemmod/TotemConstants.java
@@ -3,6 +3,6 @@ package com.barribob.totemmod;
 public class TotemConstants {
 	public static final String MOD_ID = "totemmod";
 	public static final String NAME = "Totem Mod";
-	public static final String VERSION = "0.1";
-	public static final String ACCEPTED_VERSIONS = "[1.14.4]";
+	public static final String VERSION = "0.3";
+	public static final String ACCEPTED_VERSIONS = "[1.16.2, 1.16.3, 1.16.4, 1.16.5]";
 }
